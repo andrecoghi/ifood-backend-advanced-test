e6e6a0d4e1338f8461d522898de5822cf0f26ea7
Andre Coghi <andrecoghi@ceb.unicamp.br>
2018-12-03 15:01:11 -0200

    changing uri

--- ifood-backend-advanced-test/src/main/java/br/com/ifood/controller/PlayListController.java
+++ ifood-backend-advanced-test/src/main/java/br/com/ifood/controller/PlayListController.java
@@ -1,7 +1,6 @@
 package br.com.ifood.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +10,17 @@ import br.com.ifood.service.SpotifyService;
 import br.com.ifood.utils.StringUtils;
 
 @RestController
-@RequestMapping("/playlist")
 public class PlayListController {
 
 	@Autowired
 	SpotifyService sptfyService;
 
-	@GetMapping("/city")
-	public ResponseModel findByCityName(@RequestParam("name") String cityName) {
+	@RequestMapping(value = "/playlist", params = "city")
+	public ResponseModel findByCityName(@RequestParam("city") String cityName) {
 		return sptfyService.getPlaylistBy(StringUtils.nomalize(cityName));
 	}
 
-	@GetMapping("/city/geo")
+	@RequestMapping(value = "/playlist", params = { "lat", "lon" })
 	public ResponseModel findByCoordinates(@RequestParam(value = "lat") Double lat,
 			@RequestParam(value = "lon") Double lon) {
 		return sptfyService.getPlaylistBy(lat, lon);
